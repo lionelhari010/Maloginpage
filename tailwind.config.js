346a287a602c9af83052a290368b7444252b7357
Hariharan S <hariharan@karomi.com>
2022-11-09 16:30:04 +0530

    update change3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryLightBlue: "hsl(205, 79%, 92%)",
+        veryLightBlueGray: "hsl(210, 36%, 96%)",
+      },
+    },
   },
   plugins: [],
 };
